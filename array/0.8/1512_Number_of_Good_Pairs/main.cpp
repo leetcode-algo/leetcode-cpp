64b90b747aabfbee6772beb3a01f26985826ccde
Ethan Yu <sohbmpethan@gmail.com>
2020-08-29 12:41:09 +0900

    1512_Number_of_Good_Pairs

---
+++ array/0.8/1512_Number_of_Good_Pairs/main.cpp
@@ -0,0 +1,96 @@
+/*
+Given an array of integers nums.
+
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+Return the number of good pairs.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+
+*/
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+public:
+    // Time complexity O(N^2)
+    // Space complexity O(N)
+    int numIdenticalPairs(vector<int> &nums) {
+        int cnt = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i + 1; j < nums.size(); ++j) {
+                 if (nums[i] == nums[j]) {
+                     ++cnt;
+                 }
+            }
+        }
+        return cnt;
+    }
+
+    // Time complexity O(N)
+    // Space complexity O(N)
+    int numIdenticalPairsMagic(vector<int> &nums) {
+        int mem[101] = {0};
+        int sum = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += mem[nums[i]];
+            ++mem[nums[i]];
+        }
+    }
+};
+
+int main() {
+    vector<int> nums;
+    string input;
+    cout << "nums = ";
+    getline(cin, input);
+
+    stringstream ss(input);
+    int n;
+    while (ss >> n) {
+        if(n < 1 || n > 100){
+            fprintf(stderr, "Constraints: 1 <= nums[i] <= 100");
+            return -1;
+        }
+        nums.push_back(n);
+    }
+
+    if (nums.size() < 1 || nums.size() > 100) {
+        fprintf(stderr, "Constraints: 1 <= nums.length <= 100");
+        return -1;
+    }
+
+    int result;
+    Solution s;
+    result = s.numIdenticalPairs(nums);
+
+    // Test
+    cout << result;
+
+    return 0;
+}
