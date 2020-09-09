db5a7aa4612b725d42ccba540f7546317a331e5e
Ethan Yu <sohbmpethan@gmail.com>
2020-09-09 20:02:55 +0900

    771_Jewels_and_Stones

---
+++ hash-table/0.8/771_Jewels_and_Stones/main.cpp
@@ -0,0 +1,71 @@
+/*
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    // Time complexity: O(J.length*S.length)
+    // Space complexity: O(1)
+    int numJewelsInStones(string J, string S) {
+        int cnt = 0;
+        for (char charS:S) {
+            for (char charJ:J) {
+                if (charS == charJ) {
+                    ++cnt;
+                    break;
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+int main() {
+    string strJ;
+    cout << "J = ";
+    getline(cin, strJ);
+
+    if (strJ.length() > 50) {
+        fprintf(stderr, "Note: S and J will consist of letters and have length at most 50.");
+        return -1;
+    }
+
+    string strS;
+    cout << "S = ";
+    getline(cin, strS);
+
+    if (strJ.length() > 50 || strS.length() > 50) {
+        fprintf(stderr, "Note: S and J will consist of letters and have length at most 50.");
+        return -1;
+    }
+
+    int result;
+    Solution s;
+    result = s.numJewelsInStones(strJ, strS);
+
+    // Test
+    cout << result << endl;
+
+    return 0;
+}
