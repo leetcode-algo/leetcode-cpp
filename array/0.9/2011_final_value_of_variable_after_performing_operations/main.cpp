35112da30268784f5e0fa0d1d19cc4a074de17c5
Ethan Yu <sohbmpethan@gmail.com>
2021-10-31 14:59:17 +0900

    2011_final_value_of_variable_after_performing_operations

---
+++ array/0.9/2011_final_value_of_variable_after_performing_operations/main.cpp
@@ -0,0 +1,71 @@
+/*
+There is a programming language with only four operations and one variable X:
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the
+final value of X after performing all the operations.
+
+Example 1:
+  Input: operations = ["--X","X++","X++"]
+  Output: 1
+    Explanation: The operations are performed as follows:
+    Initially, X = 0.
+    --X: X is decremented by 1, X =  0 - 1 = -1.
+    X++: X is incremented by 1, X = -1 + 1 =  0.
+    X++: X is incremented by 1, X =  0 + 1 =  1.
+
+Example 2:
+  Input: operations = ["++X","++X","X++"]
+  Output: 3
+  Explanation: The operations are performed as follows:
+  Initially, X = 0.
+  ++X: X is incremented by 1, X = 0 + 1 = 1.
+  ++X: X is incremented by 1, X = 1 + 1 = 2.
+  X++: X is incremented by 1, X = 2 + 1 = 3.
+
+Example 3:
+  Input: operations = ["X++","++X","--X","X--"]
+  Output: 0
+    Explanation: The operations are performed as follows:
+    Initially, X = 0.
+    X++: X is incremented by 1, X = 0 + 1 = 1.
+    ++X: X is incremented by 1, X = 1 + 1 = 2.
+    --X: X is decremented by 1, X = 2 - 1 = 1.
+    X--: X is decremented by 1, X = 1 - 1 = 0.
+
+Constraints:
+  1 <= operations.length <= 100
+  operations[i] will be either "++X", "X++", "--X", or "X--".
+
+Time complexity O(N)
+Space complexity O(1)
+*/
+
+#include <iostream>
+#include <vector>
+
+class Solution {
+ public:
+  static int finalValueAfterOperations(std::vector<std::string>& operations) {
+    int X = 0;
+
+    for (const std::string& op : operations) {
+      if (op[0] == '+' || op.back() == '+') {
+        X++;
+      } else {
+        X--;
+      }
+    }
+    return X;
+  }
+};
+
+int main() {
+  std::vector<std::string> input = {"--X", "X++", "X++"};
+
+  std::cout << Solution::finalValueAfterOperations(input) << std::endl;
+
+  return 0;
+}
